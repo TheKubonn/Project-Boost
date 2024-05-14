3cf916cd9d0f4905276c2a97255d1e39907614b9
F1ZZ <TheKubonn@hotmail.com>
2024-05-15 00:07:38 +0200

    Update Oscillator.cs

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,7 +4,8 @@ public class Oscillator : MonoBehaviour
 {
     private Vector3 _startingPos;
     [SerializeField] private Vector3 movementVector;
-    [SerializeField] [Range(0,1)] private float movementFactor;
+    [SerializeField] /*[Range(0,1)]*/ private float movementFactor;
+    [SerializeField] private float period = 2f;
     void Start()
     {
         _startingPos = transform.position;
@@ -12,6 +13,13 @@ public class Oscillator : MonoBehaviour
     
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+        
+        const float tau = Mathf.PI * 2f; // constant value of 6.283 etc..
+        float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+        
         Vector3 offset = movementVector * movementFactor;
         transform.position = _startingPos + offset;
     }
